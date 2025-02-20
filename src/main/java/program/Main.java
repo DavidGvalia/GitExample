466e8645436f8e4ab1bcc06f1ac0c5d2a5e0efc1
David Gvaliya <d.gvalia@yandex.ru>
2025-02-20 19:56:19 +0300

    Update x in method main, branch dev

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String message = "Hello!";
         System.out.println(message);
-        int x = 10;
+        int x = 100;
     }
     public void method(){
         System.out.println("Привет из удаленки!");
